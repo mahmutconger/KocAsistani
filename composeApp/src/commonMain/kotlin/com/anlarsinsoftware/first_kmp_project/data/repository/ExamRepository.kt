9903bd64ee41b675403c5fa3864ab8a7e833247f
Mahmut Can Conger <mahmutconger@gmail.com>
2026-04-12 22:19:32 +0300

    Updated ExamRepository to use RemoteConfigService with caching

--- composeApp/src/commonMain/kotlin/com/anlarsinsoftware/first_kmp_project/data/repository/ExamRepository.kt
+++ composeApp/src/commonMain/kotlin/com/anlarsinsoftware/first_kmp_project/data/repository/ExamRepository.kt
@@ -1,54 +1,23 @@
 package com.anlarsinsoftware.first_kmp_project.data.repository
 
 import com.anlarsinsoftware.first_kmp_project.data.model.AppConfig
-import com.anlarsinsoftware.first_kmp_project.data.model.Exam
-import com.anlarsinsoftware.first_kmp_project.data.model.Lesson
+import com.anlarsinsoftware.first_kmp_project.data.remote.RemoteConfigService
 
 class ExamRepository {
+    private val remoteConfigService = RemoteConfigService()
 
-    // İLERDE BURASI REMOTE CONFIG OLACAK
-    // Şimdilik hızlı ilerlemek için manuel listeyi buraya gömüyoruz.
+    // Cache mekanizması: Her seferinde internete gitmesin, hafızada tutsun.
+    private var cachedConfig: AppConfig? = null
+
     suspend fun getExamConfig(): AppConfig {
-        return AppConfig(
-            exams = listOf(
-                Exam(
-                    id = "YKS",
-                    name = "YKS (TYT-AYT)",
-                    date = "2026-06-20",
-                    lessons = listOf(
-                        Lesson("tyt_mat", "TYT Matematik", listOf("Temel Kavramlar", "Problemler")),
-                        Lesson("ayt_mat", "AYT Matematik", listOf("Türev", "İntegral")),
-                        Lesson("tyt_turkce", "TYT Türkçe", listOf("Paragraf", "Dil Bilgisi"))
-                    )
-                ),
-                Exam(
-                    id = "DGS",
-                    name = "Dikey Geçiş Sınavı",
-                    date = "2026-06-30",
-                    lessons = listOf(
-                        Lesson("dgs_sayisal", "Sayısal (Mat-Geo)", listOf("Sayılar", "Problemler", "Sayısal Mantık")),
-                        Lesson("dgs_sozel", "Sözel (Türkçe)", listOf("Sözcükte Anlam", "Sözel Mantık"))
-                    )
-                ),
-                Exam(
-                    id = "LGS",
-                    name = "Liselere Geçiş Sistemi",
-                    date = "2026-06-06",
-                    lessons = listOf(
-                        Lesson("lgs_mat", "Matematik", listOf("Çarpanlar", "Üslü Sayılar")),
-                        Lesson("lgs_fen", "Fen Bilimleri", listOf("Mevsimler", "DNA"))
-                    )
-                ),
-                Exam(
-                    id = "KPSS",
-                    name = "KPSS Lisans",
-                    date = "2026-07-14",
-                    lessons = listOf(
-                        Lesson("kpss_gy", "Genel Yetenek", listOf("Matematik", "Türkçe")),
-                        Lesson("kpss_gk", "Genel Kültür", listOf("Tarih", "Coğrafya"))
-                    )
-                )
-            )
-        )
+        if (cachedConfig == null) {
+            cachedConfig = remoteConfigService.fetchAppConfig()
+        }
+        return cachedConfig!!
+    }
+
+    // Config'i zorla yenilemek istersen (Örn: "Yenile" butonuyla)
+    fun clearCache() {
+        cachedConfig = null
     }
 }
