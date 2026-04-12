33be70846275199aab51348eb1dbbfa3b81a7d90
Mahmut Can Conger <mahmutconger@gmail.com>
2026-04-12 22:19:06 +0300

    Deleted Greeting and StudentProfileViewModel

--- composeApp/src/commonMain/kotlin/com/anlarsinsoftware/first_kmp_project/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.anlarsinsoftware.first_kmp_project
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
